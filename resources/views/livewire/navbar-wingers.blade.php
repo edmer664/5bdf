52f12ddeecd5185bdb8001e32111cd256bdcb34c
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-19 23:46:57 +0800

    navbar

---
+++ resources/views/livewire/navbar-wingers.blade.php
@@ -0,0 +1,42 @@
+<div>
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container-xl">
+            <div class="row justify-content-between w-100">
+                <div class="col">
+                    <a class="navbar-brand" href="/">
+                        <img src="{{asset('assets/images/Logo5BDF.png')}}" alt="" class="n-logo-sizer">
+                    </a>
+                </div>
+                <div class="col text-end">
+
+
+                    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#mobile-collapse" aria-controls="mobile-collapse" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end align-items-center h-100" id="mobile-collapse">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Brands</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Careers</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Events</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">CSR</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Our Group</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</div>
