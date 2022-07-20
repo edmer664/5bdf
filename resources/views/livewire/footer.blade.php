7fc3c9c6a40ecaa5d3497afbc1adf6ed7726cb43
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-20 21:59:42 +0800

    Home Page

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,47 @@
+<div class="bg-green py-5 text-white">
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-md-6">
+                <a class="navbar-brand" href="/">
+                    <img src="{{asset('assets/images/5bdfLogo.png')}}" alt="" class="n-logo-sizer">
+                </a>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <u>Lorem Ipsum</u>
+                        <ul>
+                            <li>Lorem Ipsum</li>
+                            <li>Lorem Ipsum</li>
+                            <li>Lorem Ipsum</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4">
+                        <u>Lorem Ipsum</u>
+                        <ul>
+                            <li>Lorem Ipsum</li>
+                            <li>Lorem Ipsum</li>
+                            <li>Lorem Ipsum</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4">
+                        <u>Lorem Ipsum</u>
+                        <ul>
+                            <li>Lorem Ipsum</li>
+                            <li>Lorem Ipsum</li>
+                            <li>Lorem Ipsum</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="text-white">
+        <div class="row">
+            <div class="col">
+                <small>
+                    &copy; 2020 5BDF. All rights reserved.
+                </small>
+            </div>
+        </div>
+    </div>
+</div>
