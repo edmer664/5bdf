1022e419938427df88f9865fe8c1bd678943b8a3
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-21 21:11:17 +0800

    wingers homepage

---
+++ app/Http/Livewire/ContactDetails.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ContactDetails extends Component
+{
+    public function render()
+    {
+        return view('livewire.contact-details');
+    }
+}
--- resources/views/5bdf/our_group.blade.php
+++ resources/views/5bdf/our_group.blade.php
@@ -151,57 +151,7 @@
     </section>
 
     {{-- Contact Details --}}
-    <section class="py-5">
-        <div class="container-xl">
-            <div class="row">
-                <div class="col-md-4">
-                    {{-- circle map icon orange bg --}}
-                    <div class="text-center">
-                        <div
-                            class="rounded-circle bg-orange circler mx-auto d-flex justify-content-center align-items-center">
-                            <i class="bi bi-geo-alt text-white fs-1 "></i>
-                        </div>
-                        <h3 class="mt-3 fw-bold">
-                            Address
-                        </h3>
-                        <p>
-                            A108 Adam Street, New York, NY 535022
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    {{-- circle map icon orange bg --}}
-                    <div class="text-center">
-                        <div
-                            class="rounded-circle bg-orange circler mx-auto d-flex justify-content-center align-items-center">
-                            <i class="bi bi-envelope text-white fs-1 "></i>
-                        </div>
-                        <h3 class="mt-3 fw-bold">
-                            Email
-                        </h3>
-                        <p>
-                            contact@example.com
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    {{-- circle map icon orange bg --}}
-                    <div class="text-center">
-                        <div
-                            class="rounded-circle bg-orange circler mx-auto d-flex justify-content-center align-items-center">
-                            <i class="bi bi-telephone text-white fs-1 "></i>
-                        </div>
-                        <h3 class="mt-3 fw-bold">
-                            Call Us
-                        </h3>
-                        <p>
-                            +1 (234) 567-890-1234
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <livewire:contact-details />
 
 
     {{-- Footer --}}
