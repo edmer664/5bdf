dc523ddbb4fcce421187eb3979ad00592c9f5f46
Kym Alcansado Ape <kymalcansadoape@gmail.com>
2022-07-21 13:18:52 +0800

    minor edit

--- resources/views/livewire/wingers-navbar.blade.php
+++ resources/views/livewire/wingers-navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container-xl d-flex flex-column flex-sm-row justify-content-between align-items-lg-center py-4">
             <h4 class="fw-semibold m-lg-0 text-start">Wingers Unlimited</h4>
 
-        <ul class=" list-unstyled d-flex gap-3 align-items-lg-center  m-0">
+        <ul class=" list-unstyled d-flex gap-3 gap-md-5 align-items-lg-center  m-0">
             <li class="fw-5"><a class="text-decoration-none text-white" href="">Careers</a></li>
             <li class="fw-5"><a class="text-decoration-none text-white" href="">Promotions</a></li>
             <li class="fw-5"><a class="text-decoration-none text-white" href="">Franchise</a></li>
