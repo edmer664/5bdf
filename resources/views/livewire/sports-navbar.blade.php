2dfd9fcd651d45ead9ff880b1c0561450043e913
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-25 16:47:38 +0800

    fix links

--- resources/views/livewire/sports-navbar.blade.php
+++ resources/views/livewire/sports-navbar.blade.php
@@ -1,6 +1,6 @@
 <div id="wingers-navbar" class="bg-red text-white">
     <div class="container-xl d-flex flex-column flex-sm-row justify-content-between align-items-lg-center py-4">
-        <a href="{{ route('5bdf.wingers.index') }}" class="text-decoration-none text-white">
+        <a href="{{ route('5bdf.sports.index') }}" class="text-decoration-none text-white">
             <h4 class="fw-semibold m-lg-0 text-start">NY Buffalo Brads Sports Lounge</h4>
         </a>
         <ul class=" list-unstyled d-flex gap-3 gap-md-5 align-items-lg-center  m-0">
