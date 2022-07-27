051142760f18a4f920bcd66d066afb61a35dbb1d
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-27 08:53:06 +0800

    csr model

---
+++ app/Http/Controllers/CsrController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Csr;
+use Illuminate\Http\Request;
+
+class CsrController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Csr  $csr
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Csr $csr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Csr  $csr
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Csr $csr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Csr  $csr
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Csr $csr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Csr  $csr
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Csr $csr)
+    {
+        //
+    }
+}
---
+++ app/Models/Csr.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Csr extends Model
+{
+    use HasFactory;
+}
