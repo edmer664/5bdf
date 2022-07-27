45bfa4073bed70a9283193169486db6d2ddc275f
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-27 08:05:18 +0800

    career migration

---
+++ database/migrations/2022_07_25_134417_create_career_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('career_posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('branch', ['hot-wings', 'wingers', 'sports', 'main']);
+            $table->text('requirements');
+            $table->string('location');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('career_posts');
+    }
+};
