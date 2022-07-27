a48a1860a5c4a2972c7552422ff0f16a9c747471
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-27 08:07:48 +0800

    fix carousel

--- resources/views/livewire/carousel.blade.php
+++ resources/views/livewire/carousel.blade.php
@@ -1,35 +1,37 @@
 <div class="w-100 ">
-    <div id="hero" class="carousel slide" data-bs-ride="carousel">
-        <ol class="carousel-indicators">
-            @if(count($carousels) == 0)
-                <li data-bs-target="#hero" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
-            @else
-                @foreach($carousels as $carousel)
-                    <li data-bs-target="#hero" data-bs-slide-to="{{ $loop->index }}" @if($loop->first) class="active" @endif aria-label="{{ $carousel->title }}"></li>
-                @endforeach
-            @endif
-        </ol>
-        <div class="carousel-inner" role="listbox">
-            @if(count($carousels) == 0)
-            <div class="carousel-item active">
-                <img class="w-100" src="{{ asset('assets/images/img3.jpg') }}" alt="First slide">
+        <div id="hero" class="carousel slide" data-bs-ride="carousel">
+            <ol class="carousel-indicators">
+                @if(count($carousels) == 0)
+                    <li data-bs-target="#hero" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
+                @else
+                    @foreach($carousels as $carousel)
+                        <li data-bs-target="#hero" data-bs-slide-to="{{ $loop->index }}" @if($loop->first) class="active" @endif aria-label="{{ $carousel->title }}"></li>
+                    @endforeach
+                @endif
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                @if(count($carousels) == 0)
+                <div class="carousel-item active">
+                    <img class="w-100" src="{{ asset('assets/images/img3.jpg') }}" alt="First slide">
+                </div>
+                @else
+                    @foreach($carousels as $carousel)
+                        <div class="carousel-item @if($loop->first) active @endif">
+                            <div class="w-100">
+                                <img class="w-100 d-block mx-auto" style="max-height: 640px; object-fit: contain" src="{{ asset('storage/carousel/'.$carousel->path) }}" alt="{{ $carousel->name }}">
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+                
             </div>
-            @else
-                @foreach($carousels as $carousel)
-                    <div class="carousel-item @if($loop->first) active @endif">
-                        <img class="w-100" src="{{ asset('storage/carousel/'.$carousel->path) }}" alt="{{ $carousel->name }}">
-                    </div>
-                @endforeach
-            @endif
-            
+            <button class="carousel-control-prev" type="button" data-bs-target="#hero" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#hero" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#hero" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#hero" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
-</div>
