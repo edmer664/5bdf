3d0ca96c7e91819473f19f5c952b8abfdbc34413
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-07-27 08:04:36 +0800

    CRUD operations for Careers

---
+++ app/Http/Controllers/CareerPostController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CareerPost;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class CareerPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('admin.careers.index',[
+            'careers' => CareerPost::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // 
+        $career = new CareerPost();
+        $career->title = $request->title;
+        $career->description = $request->description;
+        $career->requirements = $request->requirements;
+        $career->branch = $request->branch;
+        $career->location = $request->location;
+        $career->save();
+
+        return redirect()->back()->with('success', 'Career Post Created Successfully');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CareerPost  $careerPost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CareerPost $career)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CareerPost  $careerPost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CareerPost $career)
+    {
+        //
+        return view('admin.careers.edit',[
+            'career' => $career
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CareerPost  $careerPost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CareerPost $career)
+    {
+        //
+        $career->update($request->all());
+        return redirect()->route('5bdf.admin.careers.index')->with('success', 'Career Post Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CareerPost  $careerPost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CareerPost $career)
+    {
+        //
+        Log::info('Deleting career post: '.$career->title);
+        $career->delete();
+        return redirect()->back()->with('success','Career Post Deleted Successfully');
+    }
+}
