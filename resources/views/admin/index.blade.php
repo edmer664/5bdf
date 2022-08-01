ba90f60ba699184b895dc83bfb1aa1aacf2ff927
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 16:27:20 +0800

    feat:add design to dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.auth')
 @section('heading', 'Dashboard')
 @section('content')
-  
+  <div class="container-xl">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <img src="{{asset('assets/images/5bdfLogo.png')}}" alt="" class="w-100">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h2 class="text-center">
+          Welcome to 5BDF Admin Panel!
+        </h2>
+      </div>
+    </div>
+  </div>
 @endsection
