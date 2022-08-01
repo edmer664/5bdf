8a6de538e6b67700b7bb8518c584aa5bd63eab99
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 08:41:16 +0800

    feat: pass data to views

--- app/Http/Controllers/WingersController.php
+++ app/Http/Controllers/WingersController.php
@@ -4,29 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Carousel;
+use App\Models\CareerPost;
+use App\Models\Promotion;
 
 class WingersController extends Controller
 {
     function index()
     {
-        return view('wingers.index',[
+        return view('wingers.index', [
             'carousels' => Carousel::where('branch', '=', 'wingers')->get(),
         ]);
     }
 
-    function promotions()
+    function promotions(Request $request)
     {
-        return view('wingers.promotions');
+        if ($request->has('all')) {
+            return view('wingers.promotions', [
+                'carousels' => Carousel::where('branch', '=', 'wingers')->get(),
+                'promotions' => Promotion::where('branch', '=', 'wingers')->orderBy('created_at', 'desc')->get(),
+                'all' => false,
+            ]);
+        } else {
+            return view('wingers.promotions', [
+                'carousels' => Carousel::where('branch', '=', 'wingers')->get(),
+                'promotions' => Promotion::where('branch', '=', 'wingers')->orderBy('created_at', 'desc')->take(3)->get(),
+                'all' => true,
+            ]);
+        }
     }
-    
+
     function careers()
     {
-        return view('wingers.careers.index');
+        return view('wingers.careers.index', [
+            'careers' => CareerPost::where('branch', '=', 'wingers')->get(),
+        ]);
     }
 
     function franchise()
     {
-        return view('wingers.franchise');
+        return view(
+            'wingers.franchise',
+            [
+                'carousels' => Carousel::where('branch', '=', 'wingers')->get(),
+            ]
+        );
     }
 
     function store()
