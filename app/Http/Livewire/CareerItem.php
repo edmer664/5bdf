ff62beb3e0047df284fe19102ef2f7fb2411122e
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 08:43:46 +0800

    feat:add border param

--- app/Http/Livewire/CareerItem.php
+++ app/Http/Livewire/CareerItem.php
@@ -7,11 +7,13 @@ use Livewire\Component;
 class CareerItem extends Component
 {
     public $career;
+    public $border;
 
     public function render()
     {
         return view('livewire.career-item', [
             'career' => $this->career,
+            'border' => $this->border,
         ]);
     }
 }
