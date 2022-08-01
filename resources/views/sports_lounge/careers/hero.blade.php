aac51e099dff23b88d4e6e89a493582681487ae1
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 08:47:41 +0800

    feat: use component

--- resources/views/sports_lounge/careers/hero.blade.php
+++ resources/views/sports_lounge/careers/hero.blade.php
@@ -1,47 +1,20 @@
-<section class="container-xl mt-4" id="careers">
+<section class="container-xl py-5" id="careers">
     <div>
         <h4 id="careers-title" class="display-5 fw-bold"> Careers</h4>
     </div>
-    <div class=" mt-5 gap-3 careers-grid">
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
+    @if (count($careers) > 0)
+        <div class=" mt-5 gap-3 careers-grid">
+            @foreach ($careers as $career)
+                <livewire:career-item :career="$career" />
+            @endforeach
         </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
+    @else
+        <div class="text-center py-5">
+            <h5 class="fw-bold">Unfortunately, there are no careers available at the moment.</h5>
         </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
-        </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
-        </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
-        </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
-        </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
-        </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
-        </div>
-        <div class="careers-grid-items py-2 px-3 border-orange">
-            <h5 class="fw-bold">Lorem, ipsum.</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consectetur quam, a accumsan tortor convallis commodo. </p>
-        </div>
+    @endif
 
 
-    </div>
 
 
     </div>
