8d7a66b666fbc0e95ee0888075a24dd7aabdb781
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 12:33:43 +0800

    fix:missing branch col

--- database/migrations/2022_07_31_132242_create_products_table.php
+++ database/migrations/2022_07_31_132242_create_products_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string('name');
             $table->string('image');
+            $table->string('branch');
             
         });
     }
