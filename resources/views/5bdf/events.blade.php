5c572134bfe2ed468e583be0a679799926cd5d25
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 11:26:13 +0800

    feat: replace carousel

--- resources/views/5bdf/events.blade.php
+++ resources/views/5bdf/events.blade.php
@@ -5,35 +5,7 @@
     <livewire:navbar-wingers />
 
     {{-- Carousel --}}
-    {{-- TODO:Add Connection to Admin Panel Replace Static Image --}}
-    <div class="w-100 ">
-        <div id="hero" class="carousel slide" data-bs-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-bs-target="#hero" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
-                <li data-bs-target="#hero" data-bs-slide-to="1" aria-label="Second slide"></li>
-                <li data-bs-target="#hero" data-bs-slide-to="2" aria-label="Third slide"></li>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                    <img class="w-100" src="{{ asset('assets/images/img3.jpg') }}" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="w-100" src="{{ asset('assets/images/img3.jpg') }}" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="w-100" src="{{ asset('assets/images/img3.jpg') }}" alt="Third slide">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#hero" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#hero" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-    </div>
+    <livewire:carousel :carousels="$carousels" />
 
     {{-- Events --}}
     <section class="py-5">
@@ -156,28 +128,22 @@
             </div>
             <div class="row">
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt=""
-                        srcset="">
+                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt="" srcset="">
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt=""
-                        srcset="">
+                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt="" srcset="">
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt=""
-                        srcset="">
+                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt="" srcset="">
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt=""
-                        srcset="">
+                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt="" srcset="">
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt=""
-                        srcset="">
+                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt="" srcset="">
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt=""
-                        srcset="">
+                    <img src="{{ asset('assets/images/img4.jpg') }}" class="img-fluid py-3" alt="" srcset="">
                 </div>
 
             </div>
