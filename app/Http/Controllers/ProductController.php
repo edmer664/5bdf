156e4f145356d4a809fa02c786d65b570393e61c
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 13:15:43 +0800

    fix: order query by date desc

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
     {
         //
         return view('admin.products.index', [
-            'products' => Product::all()
+            'products' => Product::orderBy('created_at', 'desc')->get()
         ]);
     }
 
