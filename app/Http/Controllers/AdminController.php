c991c32ade5d534ec5ae3520bd9a1c41f89ab7a4
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 17:05:02 +0800

    feat: settings functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     function index()
     {
         return view('admin.index');
+    }
+
+    function settings()
+    {
+        return view('admin.settings');
+    }
+
+    function update_detail(Request $request)
+    {
+        // username and email
+        $user = User::find(Auth::user()->id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+        return redirect()->back()->with('success', 'Profile updated successfully');
+    }
+
+    function update_password(Request $request)
+    {
+        // password
+        $user = User::find(Auth::user()->id);
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return redirect()->back()->with('success', 'Password updated successfully');
     }
 }
