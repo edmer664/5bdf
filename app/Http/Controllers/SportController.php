8f284997ad33a3cdc5111356c8af76332a6cf42f
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 14:22:07 +0800

    feat:add data to view

--- app/Http/Controllers/SportController.php
+++ app/Http/Controllers/SportController.php
@@ -4,26 +4,54 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Carousel;
+use App\Models\CareerPost;
+use App\Models\Promotion;
+use App\Models\Product;
 
 class SportController extends Controller
 {
     //
-    function index(){
+    function index(Request $request){
+        if ($request->has('all')) {
+            return view('sport.index', [
+                'carousels' => Carousel::where('branch', '=', 'sport')->get(),
+                'products' => Product::where('branch', '=', 'sport')->orderBy('created_at', 'desc')->get(),
+            ]);
+        }
         return view('sports_lounge.index',[
-            'carousels' => Carousel::where('branch', '=', 'sports')->get(),
+            'carousels' => Carousel::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->get(),
+            'products' => Product::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->take(6)->get(),
         ]);
     }
 
-    function promotions(){
-        return view('sports_lounge.promotions');
+    function promotions(Request $request){
+        if($request->has('all')){
+            return view('sports_lounge.promotions',[
+                'carousels' => Carousel::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->get(),
+                'promotions' => Promotion::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->get(),
+                'all' => false,
+            ]);
+        }else{
+            return view('sports_lounge.promotions',[
+                'carousels' => Carousel::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->get(),
+                'promotions' => Promotion::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->take(3)->get(),
+                'all' => true,
+            ]);
+        }
     }
 
     function careers(){
-        return view('sports_lounge.careers.index');
+        return view('sports_lounge.careers.index',[
+            'careers' => CareerPost::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->get(),
+        
+        ]);
     }
 
     function franchise(){
-        return view('sports_lounge.franchise');
+        return view('sports_lounge.franchise',[
+            'carousels' => Carousel::where('branch', '=', 'sports')->orderBy('created_at', 'desc')->get(),
+        
+        ]);
     }
 
     function store(){
