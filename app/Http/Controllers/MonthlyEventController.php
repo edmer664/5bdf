baea41e0f900180ef04fdcf21c463c612f402265
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-08-01 09:56:02 +0800

    init

---
+++ app/Http/Controllers/MonthlyEventController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MonthlyEvent;
+use Illuminate\Http\Request;
+
+class MonthlyEventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MonthlyEvent  $monthlyEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MonthlyEvent $monthlyEvent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MonthlyEvent  $monthlyEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MonthlyEvent $monthlyEvent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MonthlyEvent  $monthlyEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MonthlyEvent $monthlyEvent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MonthlyEvent  $monthlyEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MonthlyEvent $monthlyEvent)
+    {
+        //
+    }
+}
