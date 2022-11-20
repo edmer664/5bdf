49efb7f277f0ebfecfb97f06ae7e88707d17e8ff
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-11-20 14:26:15 +0800

    feat: add rich text

--- resources/views/admin/careers/index.blade.php
+++ resources/views/admin/careers/index.blade.php
@@ -12,7 +12,7 @@
                         @csrf
                         <div class="form-group">
                             <label for="title">Job Title</label>
-                            <input type="text" class="form-control" name="title" id="title" placeholder="Marketer"
+                            <input type="text" class="form-control" name="title" id="title" placeholder="Title"
                                 required>
                         </div>
                         <div class="form-group">
@@ -26,18 +26,29 @@
                         </div>
                         <div class="form-group">
                             <label for="description">Description</label>
-                            <textarea class="form-control" name="description" id="description" rows="3" required></textarea>
+                            {{-- quill js --}}
+                            <div id="editor">
+                                <p>Hello World!</p>
+                                <p>Some initial <strong>bold</strong> text</p>
+                                <p><br></p>
+                            </div>
+
                         </div>
                         <div class="form-group">
                             <label for="requirements">Requirements</label>
-                            <textarea class="form-control" name="requirements" id="requirements" rows="3" required></textarea>
+                            {{-- quill js2 --}}
+                            <div id="editor2">
+                                <p>Hello World!</p>
+                                <p>Some initial <strong>bold</strong> text</p>
+                                <p><br></p>
+                            </div>
                         </div>
                         <div class="form-group">
                             <label for="location">Location</label>
-                            <input type="text" class="form-control" name="location" id="location" placeholder="Jakarta"
+                            <input type="text" class="form-control" name="location" id="location" placeholder="Location"
                                 required>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" id="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
             </div>
@@ -76,7 +87,7 @@
                                             {{ $career->created_at->format('d M Y') }}
                                         </td>
                                         <td>
-                                            @if($career->brand == 'hot-wings')
+                                            @if ($career->brand == 'hot-wings')
                                                 Hot Wings
                                             @elseif($career->brand == 'sports')
                                                 Sports Lounge
@@ -110,4 +121,77 @@
                 </div>
             </div>
         </div>
-    @endsection
+    </div>
+
+@endsection
+
+
+@push('scripts')
+    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+
+    <script>
+        // quill js
+        var quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+
+        // quill js2
+        var quill2 = new Quill('#editor2', {
+            theme: 'snow'
+        });
+
+        // on submit send an ajax request to the server
+        // using vanilla js
+        document.getElementById('submit').addEventListener('click', function(e) {
+            e.preventDefault();
+            // stop reading multiple clicks
+            document.getElementById('submit').disabled = true;
+
+            var title = document.getElementById('title').value;
+            var brand = document.getElementById('brand').value;
+            var description = quill.getContents();
+            var requirements = quill2.getContents();
+            var location = document.getElementById('location').value;
+
+            var data = {
+                title: title,
+                brand: brand,
+                description: JSON.stringify(description),
+                requirements: JSON.stringify(requirements),
+                location: location
+            }
+
+            var formData = new FormData();
+            formData.append('title', title);
+            formData.append('brand', brand);
+            formData.append('description', JSON.stringify(description));
+            formData.append('requirements', JSON.stringify(requirements));
+            formData.append('location', location);
+
+            const URL = "{{ route('5bdf.admin.careers.store') }}";
+            fetch(URL, {
+                    method: 'POST',
+                    headers: {
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute(
+                            'content'),
+                    },
+                    body: formData
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    
+                    window.location.reload();
+                    
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    // unblock the button
+                    document.getElementById('submit').disabled = false;                    
+                });
+
+
+        })
+    </script>
+@endpush
