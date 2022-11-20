717d146330e7c56b9105a75fb939cd4d7e033b1b
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-11-20 20:48:22 +0800

    feat: preview description

--- resources/views/livewire/career-item.blade.php
+++ resources/views/livewire/career-item.blade.php
@@ -1,15 +1,21 @@
-
-<a href="{{route('5bdf.career_view',$career->id)}}" class="text-decoration-none text-dark">
-    <div class="careers-grid-items py-2 px-3 
-    @if($border == 'yellow')
-        border-yellow
+<a href="{{ route('5bdf.career_view', $career->id) }}" class="text-decoration-none text-dark">
+    <div
+        class="careers-grid-items py-2 px-3 
+    @if ($border == 'yellow') border-yellow
     @elseif($border == 'orange')
         border-orange
     @elseif($border == 'grey')
-        border-grey
+        border-grey 
+    @elseif($border == 'red')
+        border-red
     @endif
     ">
         <h5 class="fw-bold">{{ $career->title }}</h5>
-        <p>{{ Str::limit($career->description, 100, '...') }}</p>
+        <p>
+            @foreach (json_decode($career->description) as $e)
+                {{ // first 200 characters
+                    substr($e[0]->insert, 0, 150) . '...' }}
+            @endforeach
+        </p>
     </div>
 </a>
